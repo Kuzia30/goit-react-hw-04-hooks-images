8e466e51608d3b17f18e72c80a56ae0831f9ff16
Kuzia30 <maxzimka37@gmail.com>
2022-01-23 21:31:40 +0200

    created button load more

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -7,24 +7,38 @@ import { List } from "./ImageGallery.styled";
 
 class ImageGallery extends Component {
   state = {
-    pictures: null,
+    pictures: [],
     status: "idle",
     error: null,
+    page: 1,
   };
 
   async componentDidUpdate(prevProps, prevState) {
-    if (prevProps.searchName !== this.props.searchName) {
+    if (
+      prevProps.searchName !== this.props.searchName ||
+      prevState.page !== this.state.page
+    ) {
       try {
         this.setState({ status: "pending" });
-        const pictures = await pixabayApi(this.props.searchName);
+        const pictures = await pixabayApi(
+          this.props.searchName,
+          this.state.page
+        );
         if (pictures.total === 0) {
           return await Promise.reject(new Error("Try another name"));
         }
-        this.setState({ pictures: pictures.hits, status: "resolved" });
+        this.setState((prevState) => ({
+          pictures: [...prevState.pictures, ...pictures.hits],
+          status: "resolved",
+        }));
       } catch (error) {
         this.setState({ status: "rejected", error: error.message });
       }
     }
+  }
+
+  handleLoadMore() {
+    this.setState((prevState) => ({ page: prevState.page + 1 }));
   }
 
   render() {
@@ -35,13 +49,28 @@ class ImageGallery extends Component {
 
     if (status === "resolved") {
       return (
-        <List>
-          <ImageGalleryItem pictures={pictures} />
-        </List>
+        <>
+          <List>
+            <ImageGalleryItem pictures={pictures} />
+          </List>
+          <button type="button" onClick={() => this.handleLoadMore()}>
+            Load more
+          </button>
+        </>
       );
     }
 
     if (status === "pending") {
+      if (this.state.pictures.length !== 0) {
+        return (
+          <>
+            <List>
+              <ImageGalleryItem pictures={pictures} />
+            </List>
+            <Oval color="#00BFFF" height={80} width={80} />
+          </>
+        );
+      }
       return <Oval color="#00BFFF" height={80} width={80} />;
     }
     if (status === "rejected") {
